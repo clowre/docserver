46d11896a8ed2676c13ba92616876f705c24b2c7
Jay <jay@sarvika.com>
2022-03-22 22:53:20 +0530

    removed OpenAPI YAML spec controller

--- src/main/java/io/sarvika/clowre/docserver/transport/SwaggerSpecsController.java
+++ src/main/java/io/sarvika/clowre/docserver/transport/SwaggerSpecsController.java
@@ -1,9 +1,7 @@
 package io.sarvika.clowre.docserver.transport;
 
-import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
-import io.micronaut.http.annotation.Produces;
 import io.sarvika.clowre.docserver.resources.SwaggerResource;
 import io.swagger.v3.oas.models.OpenAPI;
 import jakarta.inject.Inject;
@@ -16,12 +14,6 @@ public class SwaggerSpecsController {
 
     @Get("/docs.json")
     OpenAPI getDocsJOSN() {
-        return swaggerResource.read();
-    }
-
-    @Get("/docs.yaml")
-    @Produces(MediaType.APPLICATION_YAML)
-    OpenAPI getDocsYAML() {
         return swaggerResource.read();
     }
 }
